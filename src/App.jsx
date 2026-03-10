c6fd580cea392ef1b0ae1d9ed6c089243550fd11
Tobiscus100 <adeyinkaoluwatobiloba02@gmail.com>
2026-03-10 13:12:52 +0100

    Added Hero section to portfolio

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Hi, I'm Adeyinka Oluwatobiloba 👋</h1>
+      <h2>Full-Stack Developer (React + Django)</h2>
+      <p>Building amazing web applications and fintech projects.</p>
+    </div>
+  );
+     
+  
+}
+
+export default App;
