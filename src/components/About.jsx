1e9b9ea383615a6b82919c1c777e5a4418e20fd4
Tobiscus100 <adeyinkaoluwatobiloba02@gmail.com>
2026-04-03 10:08:37 +0100

    Updated About

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ function About() {
     <div className="container">
       <h2>About Me</h2>
       <p>
-        Hi! I'm Adeyinka Oluwatobiloba, a Full-Stack Developer skilled in React and Django.
+        Hi I'm Adeyinka Oluwatobiloba, a Full-Stack Developer skilled in React and Django.
         I build web applications with clean code and intuitive design, focusing on fintech and secure systems.
       </p>
       <p>
